b3db1ed3a97371d273c80248c83a866b46d1b120
saifullahsiddique <info@saifullah.co>
2022-04-04 20:08:50 +0600

    design header and hero section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,33 @@
-(function($) {
-   // Your Code
+const openIcon = document.getElementById('open')
+const closeIcon = document.getElementById('close')
+const menubar = document.querySelector('ul')
 
+function load_events() {
+    openIcon.addEventListener('click', openToggle)
+    closeIcon.addEventListener('click', closeToggle)
+}
+load_events()
 
+function openToggle() {
+    openIcon.style.display = 'none'
+    closeIcon.style.display = 'block'
 
+    menubar.style.visibility = 'visible'
+}
 
+function closeToggle() {
+    closeIcon.style.display = 'none'
+    openIcon.style.display = 'block'
 
+    menubar.style.visibility = 'hidden'
+}
 
-   
-})(jQuery);
+// Hero Slider
+$('.hero-slider').slick({
+    arrows: false,
+    dots: false,
+    enterMode: true,
+    autoplay: true,
+    infinite: true,
+    speed: 300,
+});
